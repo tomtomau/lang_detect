de1e4c1127b1ac4b7db41143df4ffb15bc305bf8
Tom Newby <tom.newby.au@gmail.com>
2013-12-03 20:23:35 +1000

    Started structure of lang_detect

--- lang_detect.py
+++ lang_detect.py
@@ -1,2 +1,19 @@
 # Language detection
 
+class Detector:
+	def __init__(self, dict_path):
+		"""
+		Constructor for detector object, takes in path to dictionary as argument
+		"""
+		self.dictionary = dict() #TODO: read file and parse into dictionary
+
+	def detect(self, text):
+		"""
+		Parse a text segment and respond with match percentage
+		"""
+		score = 0
+		# for each word
+			# if word in dictionary
+				# increase score
+
+		# return percentage score
